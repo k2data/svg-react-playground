bd233268d4635041debaa37d6b38f10f83f96fbe
fxghqc <konxiao@gmail.com>
2017-05-10 15:00:18 +0800

    line13

--- src/components/Subways/Subways.js
+++ src/components/Subways/Subways.js
@@ -30,10 +30,55 @@ export class Subways extends Component {
     // const width = +svg.attr('width')
     // const height = +svg.attr('height')
 
-    svg.append('path')
-      .attr('d', 'M3766 3698 L3766 3500')
-      .attr('stroke', '#ede611')
-      .attr('stroke-width', '10')
+    // Line 13:
+    const line13 = svg.append('g')
+    line13.append('path')
+      // x/y the same, x/y +- X (30 ~ 70)
+      .attr('d', `M3767 3698 C3727 3698, 3654 3667, 3654 3627 V2580
+        C3654 2517, 3722 2453, 3785 2453 H5318
+        C5398 2453, 5452 2524, 5452 2604 V3620
+        C5452 3670, 5411 3699, 5362 3699
+        `)
+      // .attr('stroke', '#ede611')
+      .attr('stroke', 'rgba(50, 46, 226, .7)')
+      .attr('fill', 'rgba(0, 0, 0, 0)')
+      // .attr('transform', 'translate(0, 20)')
+      .attr('stroke-width', '37')
+    const dataOfLine13 = [
+      { x: 3767, y: 3698, name: '西直门', isExchange: true, to: ['Line2', 'Line4'] },
+      { x: 3654, y: 3440, name: '大钟寺' },
+      { x: 3654, y: 3191, name: '知春路', isExchange: true, to: ['Line10'] },
+      { x: 3654, y: 3002, name: '五道口' },
+      { x: 3654, y: 2838, name: '上地' },
+      { x: 3654, y: 2584, name: '西二旗', isExchange: true, to: ['LineCHANGPING'] },
+      { x: 3851.5, y: 2453, name: '龙泽' },
+      { x: 4074.5, y: 2453, name: '回龙观' },
+      { x: 4388, y: 2453, name: '霍营', isExchange: true, to: ['Line8'] },
+      { x: 5049, y: 2453, name: '立水桥', isExchange: true, to: ['Line5'] },
+      { x: 5452, y: 2606, name: '北苑' },
+      { x: 5452, y: 2910, name: '望京西', isExchange: true, to: ['Line15'] },
+      { x: 5452, y: 3191, name: '芍药居', isExchange: true, to: ['Line10'] },
+      { x: 5452, y: 3394, name: '光熙门' },
+      { x: 5452, y: 3532, name: '柳芳' },
+      { x: 5362, y: 3699, name: '东直门', isExchange: true, to: ['Line2', 'AirportExpress'] }
+    ]
+    const stations = line13.selectAll('circle')
+      .data(dataOfLine13)
+      .enter()
+      .append('g')
+      .attr('class', styles['station'])
+
+    stations.append('circle')
+      .attr('cx', (d) => d.x)
+      .attr('cy', (d) => d.y)
+      .attr('r', 22)
+      .attr('class', styles['station--circle'])
+
+    stations.append('text')
+      .attr('x', (d) => d.x - 40)
+      .attr('y', (d) => d.y)
+      .attr('class', styles['station--text'])
+      .text((d) => d.isExchange ? '' : d.name)
   }
 
   handleClickOnCanvas (e: MouseEvent) {
