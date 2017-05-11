e2ccb864e54d0d920949831ed9a66baa27ffa916
kkstrive <kkstrive0124@gmail.com>
2017-05-11 16:33:53 +0800

    add img

--- src/components/Subways/Subways.js
+++ src/components/Subways/Subways.js
@@ -242,6 +242,7 @@ export class Subways extends Component {
       <div className={styles['canvas']} ref='canvas'
         onClick={this.handleClickOnCanvas}
       >
+        <img src='/images/subway_map.jpg' className={styles['img-layer']} />
         <svg className={styles['svg-layer']}
           xmlns='http://www.w3.org/2000/svg'
           xmlnsXlink='http://www.w3.org/1999/xlink'
